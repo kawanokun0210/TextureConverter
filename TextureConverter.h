491049348a11a5adc0f42a440d1355e3417b319e
kawanokun0210 <115131957+kawanokun0210@users.noreply.github.com>
2024-10-24 12:31:55 +0900

    コメント文追加

--- TextureConverter.h
+++ TextureConverter.h
@@ -5,12 +5,15 @@ class TextureConverter
 {
 public:
 
+	//テクスチャをWICからDDSに変換する
 	void ConvertTectureWiCToDDs(const std::string& filePath);
-
-	void LoadWICTextureFromFile(const std::string& filePath);
 
 private:
 
+	//テクスチャファイル読み込み
+	void LoadWICTextureFromFile(const std::string& filePath);
+
+	//マルチバイト文字列をワイド文字列に変換
 	static std::wstring ConvertMultiByteStringToWideString(const std::string& mString);
 
 };
