513a8b79078d98d86b39e4b969bf333b8ce8aafd
kawanokun0210 <115131957+kawanokun0210@users.noreply.github.com>
2024-10-24 13:23:09 +0900

    ファイル名分離まで終了

--- TextureConverter.cpp
+++ TextureConverter.cpp
@@ -20,6 +20,9 @@ void TextureConverter::LoadWICTextureFromFile(const std::string& filePath) {
 	HRESULT result = LoadFromWICFile(wFilePath.c_str(), WIC_FLAGS_NONE, &metadata_, scratchImage_);
 	assert(SUCCEEDED(result));
 
+	//フォルダパスとファイル名を分離する
+	SeparateFilePath(wFilePath);
+
 }
 
 std::wstring TextureConverter::ConvertMultiByteStringToWideString(const std::string& mString) {
@@ -35,4 +38,51 @@ std::wstring TextureConverter::ConvertMultiByteStringToWideString(const std::str
 	MultiByteToWideChar(CP_ACP, 0, mString.c_str(), -1, &wString[0], filePathBufferSize);
 
 	return wString;
+}
+
+void TextureConverter::SeparateFilePath(const std::wstring& filePath) {
+
+	size_t pos1;
+	std::wstring exceptExt;
+
+	//区切り文字'.'が出てくる一番最後の部分を検索
+	pos1 = filePath.rfind('.');
+	//検索がヒットしたら
+	if (pos1 != std::wstring::npos) {
+		//区切り文字の後ろをファイル拡張子として保存
+		fileExt_ = filePath.substr(pos1 + 1, filePath.size() - pos1 - 1);
+		//区切り文字の前までを抜き出す
+		exceptExt = filePath.substr(0, pos1);
+	}
+	else {
+		fileExt_ = L"";
+		exceptExt = filePath;
+	}
+
+	//区切り文字'\\'が出てくる一番最後の部分を検索
+	pos1 = exceptExt.rfind('\\');
+	//検索がヒットしたら
+	if (pos1 != std::wstring::npos) {
+		//区切り文字の前までをディレクトリパスとして保存
+		directoryPath_ = exceptExt.substr(0, pos1 + 1);
+		//区切り文字の後ろをファイル名として保存
+		fileName_ = exceptExt.substr(pos1 + 1, exceptExt.size() - pos1 - 1);
+		return;
+	}
+	
+	//区切り文字'/'が出てくる一番最後の部分を検索
+	pos1 = exceptExt.rfind('/');
+	//検索がヒットしたら
+	if (pos1 != std::wstring::npos) {
+		//区切り文字の前までをディレクトリパスとして保存
+		directoryPath_ = exceptExt.substr(0, pos1 + 1);
+		//区切り文字の後ろをファイル名として保存
+		fileName_ = exceptExt.substr(pos1 + 1, exceptExt.size() - pos1 - 1);
+		return;
+	}
+
+	//区切り文字ガないのでファイル名のみとして扱う
+	directoryPath_ = L"";
+	fileName_ = exceptExt;
+
 }
--- TextureConverter.h
+++ TextureConverter.h
@@ -17,10 +17,22 @@ private:
 	//マルチバイト文字列をワイド文字列に変換
 	static std::wstring ConvertMultiByteStringToWideString(const std::string& mString);
 
+	//フォルダパスとファイル名を分離する
+	void SeparateFilePath(const std::wstring& filePath);
+
+private:
+
 	//画像の情報
 	DirectX::TexMetadata metadata_;
 	//画像イメージのコンテナ
 	DirectX::ScratchImage scratchImage_;
+
+	//ディレクトリパス
+	std::wstring directoryPath_;
+	//ファイル名
+	std::wstring fileName_;
+	//ファイル拡張子
+	std::wstring fileExt_;
 
 };
 
